81595fec7efd3dad086a40bd5b12f9007347a74c
Tyler Grissom <minestein123@gmail.com>
2018-08-03 19:18:10 -0500

    Added build badge, basic plugin structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Havenspawns
 
+[![Build Status](https://travis-ci.com/grisstyl/Havenspawns.svg?token=TSz94qoPczfF4z3iJvmq&branch=master)](https://travis-ci.com/grisstyl/Havenspawns)
+
 Limits mob spawns near players. For user Talmore#2059 on Discord.
--- src/main/java/me/tylergrissom/havenspawns/HavenspawnsPlugin.java
+++ src/main/java/me/tylergrissom/havenspawns/HavenspawnsPlugin.java
@@ -1,7 +1,34 @@
 package me.tylergrissom.havenspawns;
 
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Level;
+
 /**
  * Copyright Tyler Grissom 2018
  */
-public class HavenspawnsPlugin {
+public class HavenspawnsPlugin extends JavaPlugin {
+
+    @Getter
+    private HavenspawnsPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+        if (!new File(getDataFolder(), "config.yml").exists()) {
+            Bukkit.getLogger().log(Level.INFO, "Config not found. Regenerating...");
+
+            saveDefaultConfig();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll(this);
+    }
 }
